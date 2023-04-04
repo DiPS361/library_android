6ba7e6a9684da466d1439efcb15d95b73fb956d6
Nur Mutaqin <mutaqin.02@gmail.com>
2023-04-04 11:31:02 +0700

    DipStart.java

---
+++ app/src/main/java/com/evo/mitzoom/DipsStart.java
@@ -0,0 +1,10 @@
+package com.evo.mitzoom;
+
+import android.content.Intent;
+import android.util.Log;
+
+public class DipsStart {
+    public static void logDebug() {
+        Log.d("DIPSTART","MASUK DIPSSSTART");
+    }
+}
