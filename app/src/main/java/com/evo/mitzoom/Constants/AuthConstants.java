6ad9a2e54a757f406b0af1b2e99cbcedc5b0d3b1
mutaqin0714 <mutaqin.02@gmail.com>
2023-04-06 14:44:24 +0700

    penurunan versi appcompat dan material

--- app/src/main/java/com/evo/mitzoom/Constants/AuthConstants.java
+++ app/src/main/java/com/evo/mitzoom/Constants/AuthConstants.java
@@ -3,6 +3,4 @@ package com.evo.mitzoom.Constants;
 public class AuthConstants {
 
     public final static String WEB_DOMAIN = "zoom.us";
-    public final static String APP_KEY = "hlcqQaGtYCVIVRirgDPVLACL8ZgItgF4RHUU";
-    public final static String APP_SECRET = "VCaGPtU6YFARCozJhloVphND0jarsJNersTG";
 }
